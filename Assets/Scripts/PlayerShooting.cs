40b23a96121855deb3c92786d9eccce60d45acbc
magur0don <magurodon.10000yen@gmail.com>
2025-06-08 12:42:16 +0900

    残弾数の表示を追加

--- Assets/Scripts/PlayerShooting.cs
+++ Assets/Scripts/PlayerShooting.cs
@@ -1,5 +1,9 @@
 using UnityEngine;
+using System.Collections;
 using UnityEngine.InputSystem;
+
+using TMPro;
+
 public class PlayerShooting : MonoBehaviour
 {
     /// <summary>
@@ -16,11 +20,44 @@ public class PlayerShooting : MonoBehaviour
     [SerializeField]
     private Transform shootPoint;
 
+    /// <summary>
+    /// 撃った弾丸数
+    /// </summary>
+    private int bulletsFired = 0;
+    /// <summary>
+    /// 最大弾数
+    /// </summary>
+    private int maxBullets = 10;
+    /// <summary>
+    /// 打ち切った場合の制限
+    /// </summary>
+    private float cooldownDuration = 10f;
+
+    /// <summary>
+    /// クールダウンに使う判定
+    /// </summary>
+    private bool isCooldown = false;
+
+    [SerializeField]
+    private TextMeshProUGUI bulletText;
+
+    private void Start()
+    {
+        bulletText.text = $"{maxBullets}/{maxBullets}";
+    }
+
     public void OnAttack(InputValue value)
     {
-        // 押されていなかったら何もしない
-        if (!value.isPressed)
+        // 押されていなかったり、クールダウン中だったら何もしない
+        if (!value.isPressed || isCooldown)
         {
+            return;
+        }
+
+        // 撃った弾丸数が最大数より増えていると
+        if (bulletsFired >= maxBullets)
+        {
+            StartCoroutine(StartCooldown());
             return;
         }
 
@@ -29,13 +66,27 @@ public class PlayerShooting : MonoBehaviour
             shootPoint.position, // 生成する位置
             shootPoint.rotation // 生成する角度
             );
+
         Rigidbody bulletRigidbody = bullet.GetComponent<Rigidbody>();
         // 力を加える
         bulletRigidbody.AddForce(
             shootPoint.forward * bulletSpeed,
             ForceMode.Impulse);
+        // 撃った弾丸数を増やす
+        bulletsFired++;
+        bulletText.text = $"{maxBullets-bulletsFired}/{maxBullets}";
     }
 
 
+    private IEnumerator StartCooldown()
+    {
+        isCooldown = true;
+        Debug.Log("弾切れ！クールダウン中...");
+        yield return new WaitForSeconds(cooldownDuration);
+        bulletsFired = 0;
+        isCooldown = false;
+        Debug.Log("再装填完了！");
+        bulletText.text = $"{maxBullets}/{maxBullets}";
+    }
 
 }
